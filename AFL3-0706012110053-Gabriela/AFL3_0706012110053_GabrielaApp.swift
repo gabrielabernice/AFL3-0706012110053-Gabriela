68b6d3fac34bb37563f86b8aa27d3f53447fdeb2
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-04-30 21:27:48 +0700

    Initial Commit

---
+++ AFL3-0706012110053-Gabriela/AFL3_0706012110053_GabrielaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110053_GabrielaApp.swift
+//  AFL3-0706012110053-Gabriela
+//
+//  Created by MacBook Pro on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110053_GabrielaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
